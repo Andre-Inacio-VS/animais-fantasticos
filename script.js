7af14e206679eef79c1c5a86918f421d7b404e4e
andre.silva <ANDRE.SILVA@PROTENDIT.COM.BR>
2024-05-03 11:07:01 -0300

    exercicio 5

--- script.js
+++ script.js
@@ -116,3 +116,54 @@ const menuMobile = window.matchMedia("(max-width: 720px)").matches;
 if (menuMobile) document.querySelector(".menu").classList.add("menu-mobile");
 
 */
+
+/*
+Exercicio 5)
+
+// Quando o usuário clicar nos links internos do site,
+// adicione a classe ativo ao item clicado e remova dos
+// demais itens caso eles possuam a mesma. Previna
+// o comportamento padrão desses links
+const links = document.querySelectorAll('a[href^="#"]');
+
+function activeLink(e) {
+  links.forEach((link) => {
+    if (link.classList.contains("ativo")) link.classList.remove("ativo");
+  });
+  e.currentTarget.classList.add("ativo");
+  e.preventDefault();
+}
+
+links.forEach((link) => {
+  link.addEventListener("click", activeLink);
+});
+
+// Selecione todos os elementos do site começando a partir do body,
+// ao clique mostre exatamente quais elementos estão sendo clicados
+const todosElementos = document.querySelectorAll("body *");
+
+function showElement(e) {
+  console.log(e.currentTarget);
+}
+function removeElement(e) {
+  e.currentTarget.remove();
+}
+todosElementos.forEach((element) => {
+  // element.addEventListener("click", showElement);
+  // element.addEventListener("click", removeElement);
+});
+
+// Utilizando o código anterior, ao invés de mostrar no console,
+// remova o elemento que está sendo clicado, o método remove() remove um elemento
+
+// Se o usuário clicar na tecla (t), aumente todo o texto do site.
+
+function textScale(e) {
+  if (e.key === "t") {
+    document.documentElement.classList.toggle("scaleText");
+  }
+  e.preventDefault();
+}
+
+window.addEventListener("keydown", textScale);
+*/
