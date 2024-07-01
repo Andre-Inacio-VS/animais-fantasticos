ac02256d413a5a40d9d94ac0e74faf92f9753541
Andre-Inacio-VS <andreinaciovs@gmail.com>
2024-07-01 15:28:05 -0300

    feat (Animais Lista): adicionando funcionalidade de selecao de visualizacao dos animais e suas descricoes

--- script.js
+++ script.js
@@ -200,3 +200,23 @@ console.log(animais);
 
 faq.innerHTML = animais.innerHTML;
 */
+
+const tabmenu = document.querySelectorAll(".js-tabmenu li");
+const tabcontent = document.querySelectorAll(".js-tabcontent section");
+
+if (tabmenu.length && tabcontent.length) {
+  tabcontent[0].classList.add("ativo");
+
+  function activeTab(index) {
+    tabcontent.forEach((section) => {
+      section.classList.remove("ativo");
+    });
+    tabcontent[index].classList.add("ativo");
+  }
+
+  tabmenu.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      activeTab(index);
+    });
+  });
+}
