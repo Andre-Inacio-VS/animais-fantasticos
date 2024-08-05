b505dde2477e737c67978b64c53ed04ac601d254
andre.silva <ANDRE.SILVA@PROTENDIT.COM.BR>
2024-08-05 14:52:55 -0300

    feat Criando modal de login

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initScroll from './modules/scroll-suave.js'
 import initScrollAnimate from './modules/scroll-animate.js'
 import initTabNav from './modules/tab-nav.js'
 import initAccordion from './modules/accordion.js'
+import initModal from './modules/modal.js'
 
 initScroll();
 initScrollAnimate();
 initTabNav();
 initAccordion();
+initModal();
