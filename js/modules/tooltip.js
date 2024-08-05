1cb0519fdb41126fabdd4b855ad42581bc131566
andre.silva <ANDRE.SILVA@PROTENDIT.COM.BR>
2024-08-05 17:05:41 -0300

    feat Tooltip para as section com o data-tooltip

---
+++ js/modules/tooltip.js
@@ -0,0 +1,55 @@
+export default function initTooltip() {
+
+    const tooltips = document.querySelectorAll('[data-tooltip]')
+
+    function verTooltip(e) {
+        const tooltipBox = criarTooltip(this)
+
+        moveTooltip.tooltipBox = tooltipBox
+        moveTooltip.element = this
+        this.addEventListener('mousemove', moveTooltip)
+
+        hiddeTootlip.tooltipBox = tooltipBox
+        hiddeTootlip.element = this
+        this.addEventListener('mouseleave', hiddeTootlip)
+    }
+
+    function criarTooltip(element) {
+        const tooltipBox = document.createElement('div')
+        const text = element.getAttribute('aria-label')
+        tooltipBox.classList.add('tooltip')
+        tooltipBox.innerText = text
+        document.body.appendChild(tooltipBox)
+        return tooltipBox
+    }
+
+    const hiddeTootlip = {
+        tooltipBox: '',
+        element: '',
+        handleEvent() {
+            this.tooltipBox.remove()
+            this.element.removeEventListener('mouseleave', hiddeTootlip)
+        },
+    }
+
+    const moveTooltip = {
+        tooltipBox: '',
+        element: '',
+        handleEvent(e) {
+            this.tooltipBox.style.top = e.pageY + 20 + 'px'
+            if (e.pageX <= this.element.offsetWidth * .5) {
+                this.tooltipBox.style.left = e.pageX + 20 + 'px'
+                this.tooltipBox.style.right = 'auto'
+            } else {
+                this.tooltipBox.style.right = (window.innerWidth - e.pageX + 20) + 'px'
+                this.tooltipBox.style.left = 'auto'
+            }
+            this.element.removeEventListener('mouseleave', moveTooltip)
+        },
+    }
+
+    tooltips.forEach(item => {
+        item.addEventListener('mouseover', verTooltip)
+    });
+
+}
--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initScrollAnimate from './modules/scroll-animate.js'
 import initTabNav from './modules/tab-nav.js'
 import initAccordion from './modules/accordion.js'
 import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
 
 initScroll();
 initScrollAnimate();
 initTabNav();
 initAccordion();
 initModal();
+initTooltip();
