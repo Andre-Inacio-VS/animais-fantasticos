3fc80dbc579987a304c86c8949ae5c431272fc92
andre.silva <ANDRE.SILVA@PROTENDIT.COM.BR>
2024-08-14 15:40:25 -0300

    refact Modal: transformando em uma classe com metodos

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,21 +1,37 @@
-export default function initModal() {
+export default class Modal {
+    constructor(abrir, fechar, container) {
+        this.btnAbrir = document.querySelector(abrir);
+        this.btnFechar = document.querySelector(fechar);
+        this.containerModal = document.querySelector(container);
+        this.classActive = "ativo";
 
-    const btnAbrir = document.querySelector("[data-modal='abrir']")
-    const btnFechar = document.querySelector("[data-modal='fechar']")
-    const containerModal = document.querySelector("[data-modal='container']")
+        this.eventToggleModal = this.eventToggleModal.bind(this);
+        this.fecharModalContainer = this.fecharModalContainer.bind(this);
+    }
 
-    if (btnAbrir && btnFechar && containerModal) {
-        function toggleModal(e) {
-            e.preventDefault();
-            containerModal.classList.toggle('ativo')
-        }
+    toggleModal() {
+        this.containerModal.classList.toggle(this.classActive);
+    }
 
-        function fecharModalContainer(e) {
-            if (e.target === this) toggleModal(e)
-        }
+    eventToggleModal(event) {
+        event.preventDefault();
+        this.toggleModal();
+    }
 
-        btnAbrir.addEventListener('click', toggleModal)
-        btnFechar.addEventListener('click', toggleModal)
-        containerModal.addEventListener('click', fecharModalContainer)
+    fecharModalContainer(event) {
+        if (event.target === this.containerModal)
+            this.toggleModal(event);
+    }
+
+    addModalEvents() {
+        this.btnAbrir.addEventListener("click", this.eventToggleModal);
+        this.btnFechar.addEventListener("click", this.eventToggleModal);
+        this.containerModal.addEventListener("click", this.fecharModalContainer);
+    }
+
+    init() {
+        if (this.btnAbrir && this.btnFechar && this.containerModal)
+            this.addModalEvents();
+        return this;
     }
 }
